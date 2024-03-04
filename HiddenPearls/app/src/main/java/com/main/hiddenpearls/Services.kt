41604dfd43d16bbbf1f821b42d0e70fee0898770
Kjartan Oli Agustsson <koa20@hi.is>
2024-03-04 13:50:59 +0000

    Add caching
    Ktor has  caching support, in theory this is all we need to do to take
    advantage of it.

--- HiddenPearls/app/src/main/java/com/main/hiddenpearls/Services.kt
+++ HiddenPearls/app/src/main/java/com/main/hiddenpearls/Services.kt
@@ -6,6 +6,7 @@ import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.cache.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
@@ -20,6 +21,7 @@ object LocationService {
 		install(ContentNegotiation) {
 			json()
 		}
+		install(HttpCache)
 		defaultRequest {
 			url {
 				/* protocol = URLProtocol.HTTP */
