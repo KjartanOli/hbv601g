82cdfd0fd41b2cf4622269b2a641469af3c1a2f8
Sverrir Sigfusson <s.sigfusson@icloud.com>
2024-03-11 22:50:12 +0000

    removed unneeded code, partaining to datastore

--- HiddenPearls/app/src/main/java/com/main/hiddenpearls/MainActivity.kt
+++ HiddenPearls/app/src/main/java/com/main/hiddenpearls/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.main.hiddenpearls
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,23 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.compose.rememberNavController
 import com.main.hiddenpearls.ui.theme.HiddenPearlsTheme
-
-
-object PreferencesSerializer {
-	val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
-		name = "favorites",
-	)
-}
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		val dataStore = applicationContext.dataStore
 		FavoritesService.init(this)
 		setContent {
 			HiddenPearlsTheme {
